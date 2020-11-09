18ed3889eec1b7ae961cfb6e6cef7957161d8557
simishra <siddharth.mishra@netgear.com>
2020-11-09 07:45:18 +0000

    SEAL-241 Merge partner changes to open source ini library
    These changes are mostly whitespace (including changing spaces to tabs)
    and cleanups, but have a version bump from 0.1.1 to 0.2.0.
    If upstream changes from rxi occur in the future, this could cause a conflict
    (however, upstream has not changed in 4 years, so perhaps this case is unlikely).

--- src/ini.h
+++ src/ini.h
@@ -1,20 +1,25 @@
 /**
  * Copyright (c) 2016 rxi
  *
+ * https://github.com/rxi/ini
+ *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the MIT license. See `ini.c` for details.
  */
 
-#ifndef INI_H
-#define INI_H
+#ifndef __INI_H__
+#define __INI_H__
 
-#define INI_VERSION "0.1.1"
+#define INI_VERSION "0.2.0"
 
-typedef struct ini_t ini_t;
+typedef struct ini_t {
+	char *data;
+	char *end;
+} ini_t;
 
-ini_t*      ini_load(const char *filename);
-void        ini_free(ini_t *ini);
-const char* ini_get(ini_t *ini, const char *section, const char *key);
-int         ini_sget(ini_t *ini, const char *section, const char *key, const char *scanfmt, void *dst);
+ini_t *ini_load(const char *filename);
+void ini_free(ini_t * ini);
+const char *ini_get(ini_t * ini, const char *section, const char *key);
+int ini_sget(ini_t * ini, const char *section, const char *key, const char *scanfmt, void *dst);
 
-#endif
+#endif /* __INI_H__ */
